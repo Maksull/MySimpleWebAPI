d4524891bd7906586379cd45b12f58a839e3dc79
Maksym Yuhov <kyrkyrkyr99@gmail.com>
2022-08-07 17:06:40 +0300

    Basic configuration.

--- MyWebAPI/Program.cs
+++ MyWebAPI/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Services
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:MyWebAPI"]);
+    opts.EnableSensitiveDataLogging();
+});
+
+#endregion
+
 var app = builder.Build();
 
+#region App
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseMiddleware<MyWebAPI.TestMiddleware>();
+
+SeedData.EnsurePopulated(app);
+
+#endregion
 
 app.Run();
